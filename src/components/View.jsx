fc5a63ae8c893dfa7b4bec50248a1768c92770c4
aswinraj <aswinraj050@gmail.com>
2024-10-20 00:42:29 +0530

    third

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,74 +1,98 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import { getEmployeeApi, deleteEmployee } from '../services/allApis';
+import { getEmployeeApi } from '../services/allApis';
+import { deleteEmployee } from '../services/allApis';
+import { useEffect } from 'react';
 import Edit from './Edit';
 import { toast } from 'react-toastify';
-import { Row, Col } from 'react-bootstrap'; // Import Row and Col
+import { Row,Col } from 'react-bootstrap';
+
 
 function View() {
-  const [data, setData] = useState([]);
+  const [data,setData]=useState([])
 
-  useEffect(() => {
-    getData();
-  }, []); // Remove data from dependency array to avoid infinite loop
+  useEffect(()=>{
+    getData()
+    
+  },[data])
 
-  const getData = async () => {
-    const res = await getEmployeeApi();
-    console.log(res);
-    if (res.status === 200) {
-      setData(res.data);
-    } else {
-      console.log(res);
+
+ 
+
+
+  const getData=async()=>{
+    const res=await getEmployeeApi()
+    console.log(res)
+    if(res.status==200){
+      setData(res.data)
     }
-  };
-
-  const handleDelete = async (id) => {
-    const res = await deleteEmployee(id);
-    if (res.status === 200) {
-      console.log(res.data);
-      toast.success('Employee deleted!!');
-      getData();
-    } else {
-      console.log(res);
-      toast.error("Something went wrong!!");
+    else{
+      console.log(res)
     }
-  };
+  }
+
+
+  const handleDelete=async(id)=>{
+    const res=await deleteEmployee(id)
+    if(res.status==200){
+      console.log(res.data)
+      toast.success('Employee deleted!!')
+      getData()
+
+    }
+    else{
+      console.log(res)
+      toast.error("Something went wrong!!")
+
+    }
+  }
+
+
 
   return (
     <>
-      {data?.length > 0 ? (
-        <Row className="justify-content-center">
-          {data.map((item) => (
-            <Col xs={12} sm={6} md={4} lg={3} className="mb-4" key={item._id}> 
-              <Card className='text-center'>
-                <Card.Img 
-                  variant="top" 
-                  src="https://png.pngtree.com/png-vector/20220901/ourmid/pngtree-company-employee-avatar-icon-wearing-a-suit-png-image_6133899.png" 
-                />
-                <Card.Body>
-                  <Card.Title>{item.firstname} {item.lastname}</Card.Title>
-                  <Card.Text>
-                    <p>Age: {item.age}</p>
-                    <p>{item.qualification}</p>
-                    <p>{item.email}</p>
-                  </Card.Text>
-                  <div className='d-flex justify-content-between'>
-                    <Edit data={item} />
-                    <Button variant="primary" onClick={() => handleDelete(item._id)}>
-                      Delete <i className="fa-solid fa-trash" style={{ color: "#ff7070" }} />
-                    </Button>
-                  </div>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      ) : (
-        <h4 className='text-center text-danger'>No employees added!!</h4>
-      )}
-    </>
-  );
+
+    {
+      data?.length>0?
+<>
+{
+  data?.map(item=>(
+
+    <Card style={{ width: '18rem' }} className='text-center mx-4'>
+    <Card.Img variant="top" src="https://png.pngtree.com/png-vector/20220901/ourmid/pngtree-company-employee-avatar-icon-wearing-a-suit-png-image_6133899.png" />
+    <Card.Body>
+      <Card.Title>{item.firstname}{item.lastname}</Card.Title>
+      <Card.Text><p>Age:{item.age}</p></Card.Text>
+
+          <p>{item.qualification}</p>
+          <p>{item.email}</p>
+
+     <div className='d-flex justify-content-between'>
+             <Edit data={item}/>
+          <Button variant="primary" onClick={()=>handleDelete(item._id)}>Delete <i className="fa-solid fa-trash" style={{color: "#ff7070",}} /></Button>
+     </div>
+    </Card.Body>
+  </Card>
+
+
+
+
+  ))
+}
+  
+
+
+</>
+:
+ <h4 className='text-center text-danger'>No employees added!!</h4>
+
+    }
+  
+
+
+  </>
+  )
 }
 
-export default View;
+export default View
